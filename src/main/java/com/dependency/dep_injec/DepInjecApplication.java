1e7e6b673c688c3b0ed06c465b4f454a9ab3bd74
ShreemoyeeMukherjee <113385446+ShreemoyeeMukherjee@users.noreply.github.com>
2024-12-03 20:21:21 +0000

    connection done

--- src/main/java/com/dependency/dep_injec/DepInjecApplication.java
+++ src/main/java/com/dependency/dep_injec/DepInjecApplication.java
@@ -6,13 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.dependency.dep_injec.constructor_injection.EmailService;
 import com.dependency.dep_injec.constructor_injection.MessagingService;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 //import com.dependency.dep_injec.constructor_injection.UserNotificationService;
 
 @SpringBootApplication
 public class DepInjecApplication {
 	@Autowired
 		//private App app;
-		///private  UserService userservice;
+		//private  UserService userservice;
 		
 			public static void main(String[] args) {
 				
@@ -21,8 +24,27 @@ public class DepInjecApplication {
 				// Get the App bean from the context and call the helloService method
 				// UserService userservice = context.getBean(UserService.class);
 				// userservice.greetUser("shree");
-				MessagingService messagingService= context.getBean("emailservice",MessagingService.class);
-				messagingService.sendMessage("Hellooooooo");
+				// MessagingService messagingService= context.getBean("emailservice",MessagingService.class);
+				// messagingService.sendMessage("Hellooooooo");
+				try {
+					// Build the SessionFactory from hibernate.cfg.xml
+					SessionFactory sessionFactory = new Configuration()
+							.configure("hibernate.cfg.xml") // Config file name
+							
+							.buildSessionFactory();
+		
+					// Open a session and test the connection
+					//Session session = sessionFactory.openSession();
+					System.out.println("Hibernate connection successful!");
+		
+					//session.close();
+					sessionFactory.close();
+		
+				} catch (Exception e) {
+					System.out.println("Error: Could not establish Hibernate connection.");
+					e.printStackTrace();
+				}
+			}
 	}
 
-}
+
